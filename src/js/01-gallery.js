dece1965f872124383d83a3292f3c0530a4e1a28
Надія Ткач <nadiya77723@gmail.com>
2022-09-03 19:27:34 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,13 +1,10 @@
-
+import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-
-import SimpleLightbox from "simplelightbox";
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.min.css";
-
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const galleryContainer = document.querySelector('.gallery');
 const galleryImg = createGalleryImg(galleryItems);
