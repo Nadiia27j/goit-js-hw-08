8fd8466b1daff447643baeeafa616ddc291c9d8c
Надія Ткач <nadiya77723@gmail.com>
2022-09-03 18:57:14 +0300

    рппппп

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,49 @@
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+
+import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryImg = createGalleryImg(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryImg);
+
+console.log(createGalleryImg(galleryItems));
+
+function createGalleryImg(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+           <a class="gallery__link" href="${original}">
+           <img
+           class="gallery__image"
+           src="${preview}"
+           data-source="${original}"
+           alt="${description}"
+           />
+           </a>
+        </div>
+      `;
+    }).join("");
+};
+
+
+new SimpleLightbox ('.gallery a', { 
+  nav: true,
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+SimpleLightbox();
+ 
+
+
+
+
