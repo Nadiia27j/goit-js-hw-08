67bb28b363c046939233bfeaf421e7e19cd5a913
Надія Ткач <nadiya77723@gmail.com>
2022-09-05 20:14:53 +0300

    виконує завдання 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+let throttle = require('lodash.throttle');
+
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+let formData = {};
+
+populateInput();
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+   e.preventDefault();
+  formData[e.target.name] = e.target.value;
+
+   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    const parseMessage = JSON.parse(savedMessage);
+
+    if (parseMessage) {
+        console.log(parseMessage);
+    }
+
+    e.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+
+function populateInput() {
+ const savedMessage = localStorage.getItem(STORAGE_KEY);
+ const parseMessage = JSON.parse(savedMessage);
+ 
+ if (parseMessage) {
+    formData = JSON.parse(parseMessage);
+    email.value = formData.email || '';
+    message.value = formData.message || '';
+  }
+}
 
