0e1d183556a23c63e4feb7276a9ea83581d32798
Надія Ткач <nadiya77723@gmail.com>
2022-09-06 13:32:10 +0300

    виправляє помилки в 3 задачі

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
-let throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
@@ -9,14 +9,15 @@ populateInput();
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
+// записується у локал сторедж {} з полями
 function onFormInput(e) {
    e.preventDefault();
   formData[e.target.name] = e.target.value;
 
    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
-
-
+ 
+// під час сабміту очищається локал і поля, і в консоль виодь {} з полями.
 function onFormSubmit(e) {
     e.preventDefault();
     const savedMessage = localStorage.getItem(STORAGE_KEY);
@@ -30,15 +31,15 @@ function onFormSubmit(e) {
     localStorage.removeItem(STORAGE_KEY);
 }
 
-
+// під час завантаження сторінки перевір стан локал ,якщо є дані заповни ними поля форми
 function populateInput() {
  const savedMessage = localStorage.getItem(STORAGE_KEY);
  const parseMessage = JSON.parse(savedMessage);
  
  if (parseMessage) {
-    formData = JSON.parse(parseMessage);
-    email.value = formData.email || '';
-    message.value = formData.message || '';
+    formData = parseMessage || '';
+    form.email.value = formData.email || '';
+    form.message.value = formData.message || '';
   }
 }
 
