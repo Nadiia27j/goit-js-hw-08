e65a99e2d9590b7ca0af105b404b9a371bb19a16
Надія Ткач <nadiya77723@gmail.com>
2022-10-09 19:04:55 +0300

    .

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,35 +11,34 @@ form.addEventListener('submit', onFormSubmit);
 
 // записується у локал сторедж {} з полями
 function onFormInput(e) {
-   e.preventDefault();
+  e.preventDefault();
   formData[e.target.name] = e.target.value;
 
-   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-};
- 
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
 // під час сабміту очищається локал і поля, і в консоль виодь {} з полями.
 function onFormSubmit(e) {
-    e.preventDefault();
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
-    const parseMessage = JSON.parse(savedMessage);
+  e.preventDefault();
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const parseMessage = JSON.parse(savedMessage);
 
-    if (parseMessage) {
-        console.log(parseMessage);
-    }
+  if (parseMessage) {
+    console.log(parseMessage);
+  }
 
-    e.target.reset();
-    localStorage.removeItem(STORAGE_KEY);
+  e.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 // під час завантаження сторінки перевір стан локал ,якщо є дані заповни ними поля форми
 function populateInput() {
- const savedMessage = localStorage.getItem(STORAGE_KEY);
- const parseMessage = JSON.parse(savedMessage);
- 
- if (parseMessage) {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const parseMessage = JSON.parse(savedMessage);
+
+  if (parseMessage) {
     formData = parseMessage || '';
     form.email.value = formData.email || '';
     form.message.value = formData.message || '';
   }
 }
-
